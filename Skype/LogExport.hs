eb14b7ffb92ddae7e25bd16a8d5ef69e9c0e270b
Eugene Dzhurinsky <jdevelop@gmail.com>
2012-06-20 10:25:02 +0300

    Skip empty messages and add padding.

--- Skype/LogExport.hs
+++ Skype/LogExport.hs
@@ -16,33 +16,41 @@ import System.FilePath
 import System.IO
 import System.Locale
 
+import Control.Arrow
+
+import Debug.Trace
+
 newtype SortedSkypeEntry = SortedSkypeEntry { entries :: [SkypeEntry] }
 
 type FolderName = ByteString
 
-newtype ExportChat = ExportChat { export :: ( FolderName, SortedSkypeEntry ) }
+newtype ExportChat = ExportChat { export :: ( FolderName, (Int, SortedSkypeEntry) ) }
 
 getFolderName = fst . export
 
-getEntries = entries . snd . export
+getEntries = entries . snd . snd . export
+
+getUsernameSize = fst . snd . export
 
 sortEntries :: [SkypeEntry] -> SortedSkypeEntry
 sortEntries = SortedSkypeEntry . DS.toList . DS.fromList
 
 exportChat :: String -> SkypeChat -> ExportChat
-exportChat srcUser chat = ExportChat (folderName, sortedEntries)
+exportChat srcUser chat = ExportChat (folderName, (maxLen, sortedEntries))
     where
         sortedEntries = sortEntries . messages $ chat
         firstEntry = DL.head . entries $ sortedEntries
         srcUser' = DB8.pack srcUser
         uL = userL chat
         uR = userR chat
+        maxLen = uncurry max $ join (***) DB8.length (uL,uR)
         folderName | srcUser' == uL = uR
                  | srcUser' == uR = uL
                  | otherwise = srcUser'
 
 
 separator = DB8.pack " : "
+space = DB8.head $ DB8.pack " "
 
 exportChats :: String -> String -> [SkypeChat] -> IO ()
 exportChats folder username = mapM_ ( go . exportChat username )
@@ -65,15 +73,23 @@ exportChats folder username = mapM_ ( go . exportChat username )
                 firstEntry = DL.head . getEntries $ chat
                 firstEntryDateStr = formatTime defaultTimeLocale "%Y-%m-%d  %H-%M-%S" . timeStamp $ firstEntry
                 folderName = getFolderName chat
+                usernameSize = getUsernameSize chat
+                pad l str | len' == l = str
+                          | otherwise = DB8.concat [str, DB8.replicate ( l - len') space]
+                  where
+                    len' = DB8.length str
                 doExport handle =
                     mapM_ (writeEntry handle) $ getEntries chat
-                writeEntry handle chatEntry = do
-                    let username = senderId chatEntry
-                    let content = message chatEntry
-                    let timestamp = formatTime defaultTimeLocale " %d-%m-%Y %H:%M:%S" . timeStamp $ chatEntry
-                    -- ugly formatting goes here
-                    S.hPutStr handle username
-                    S.hPutStr handle separator 
-                    S.hPutStr handle $ DB8.pack timestamp 
-                    S.hPutStr handle separator
-                    S.hPutStrLn handle content
+                writeEntry handle chatEntry | DB8.length content == 0 = return ()
+                                            | otherwise = do
+                                                let username = pad usernameSize $ senderId chatEntry
+                                                let timestamp = formatTime defaultTimeLocale " %d-%m-%Y %H:%M:%S" . 
+                                                                timeStamp $ chatEntry
+                                                -- ugly formatting goes here
+                                                S.hPutStr handle username
+                                                S.hPutStr handle separator 
+                                                S.hPutStr handle $ DB8.pack timestamp 
+                                                S.hPutStr handle separator
+                                                S.hPutStrLn handle content
+                    where
+                      content = message chatEntry
